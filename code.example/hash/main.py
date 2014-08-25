2990e1f08dbf6d5a61feec3cc4bd39499c019370
mar3 <irisawa.masaru@gmail.com>
2014-08-26 00:25:15 +0900

    wip

---
+++ code.example/hash/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+import codecs
+import hashlib
+
+def _println(*args):
+
+	out = codecs.getwriter('utf-8')(sys.stdout)
+	for x in args:
+		out.write('' + x)
+	out.write("\n")
+
+def main():
+
+	#
+	# MD5 DIGEST
+	#
+	if True:
+		g = hashlib.md5()
+		g.update('abcdefg')
+		_println('[', g.hexdigest(), ']')
+	
+	#
+	# SHA-256
+	#
+	if True:
+		g = hashlib.sha256()
+		g.update('abcdefg')
+		_println('[', g.hexdigest(), ']')
+
+main()
+
