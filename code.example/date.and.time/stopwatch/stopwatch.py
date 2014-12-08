518e02b09dad0e6c97b4506763db3061dad86950
mass10 <irisawa.masaru@gmail.com>
2014-12-08 20:17:41 +0900

    wip

---
+++ code.example/date.and.time/stopwatch/stopwatch.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+class stopwatch:
+
+	_value = None
+
+	def __init__(self):
+		self._member = datetime.datetime.now()
+
+	def __repr__(self):
+		current = datetime.datetime.now()
+		elapsed = current - self._member
+		return str(elapsed)
