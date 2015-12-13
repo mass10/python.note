740df0939d7b5251414ee7bfd432362939d5270c
mass10 <irisawa.masaru@gmail.com>
2015-12-13 12:28:56 +0900

    wip

--- code/string/main.py
+++ code/string/main.py
@@ -2,30 +2,58 @@
 # coding: utf-8
 
 import sys
+import json
 
 def _println(*xx):
+
+	out = sys.stdout.write
+
 	for x in xx:
 		if x is None:
 			continue
-		sys.stdout.write(str(x))
-	sys.stdout.write("\n")
+		elif type(x) is int:
+			out(str(x))
+		elif type(x) is str:
+			out(x)
+		elif type(x) is dict:
+			out(json.dumps(x, ensure_ascii=False, sort_keys=True))
+		elif type(x) is list:
+			out(json.dumps(x, ensure_ascii=False, sort_keys=True))
+		else:
+			out(str(type(x)))
 
-def main():
+	out("\n")
+
+def _test_20151109():
+
+	s = u'東京都八王子市高尾町 1-1-1'
+	print u'"{}" の「八王子」の位置は ({}) です。'.format(s, s.index(u'八王子'))
+	s = u'1-1-1, Takaomachi, Hahcioji, Tokyo, Japan'
+	print u'"{}" の「1-1-1」の位置は ({}) です。'.format(s, s.index(u'1-1-1'))
+
+def _main():
 
 	#
+	# dict の表示
 	#
-	#
-	if 1:
+	if True:
 		s = {
-			u'key1': u'あいうえお'
+			u'key-1': u'あいうえお',
+			u'key-2': u'東京都八王子市高尾町 1-1-1'
 		}
 		_println(s)
 
 	#
 	# 部分文字列の取り出し
 	#
-	if 1:
+	if True:
 		s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 		_println(s[4:6])
 
-main()
+	if True:
+		a = [99999, '東京都八王子市高尾町', 123.4567]
+		_println(a);
+
+	# _test_20151109()
+
+_main()
