b3fc6159f406fde40517d8dc09ba89e0c161a8dc
mass10 <irisawa.masaru@gmail.com>
2015-12-27 18:26:47 +0900

    wip

--- code/database/elasticsearch/examples/enum.py
+++ code/database/elasticsearch/examples/enum.py
@@ -20,22 +20,39 @@ def _main(argv):
 	if 2 <= len(argv):
 		index_name = argv[1]
 
+	#
 	# 接続設定
+	#
+
 	es = Elasticsearch(host = '127.0.0.1')
 
 	if not es.indices.exists(index_name):
 		print('INDEX:{0} が存在しません'.format(index_name))
 		return
 
+	#
 	# 全件検索
-	# response = es.search(index = index_name, body = {}) # returns 10 indices.
-	response = es.search(index = index_name, body={"query": {"match_all": {}}, "size": 99999})
+	#
 
+	# response = es.search(
+	# 	index = index_name,
+	# 	body = {}) # returns 10 indices.
+
+	response = es.search(
+		index = index_name,
+		body={"query": {"match_all": {}}, "size": 9999})
+
+	#
 	# 結果を表示
+	#
+
 	for e in response['hits']['hits']:
 		print json.dumps(e, ensure_ascii=False, sort_keys=True)
 
+	#
 	# ヒット数
+	#
+
 	print('hits: {0}'.format(response['hits']['total']))
 
 _main(sys.argv)
