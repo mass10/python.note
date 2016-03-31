59fe8057d65eac8b396ad5f55bf4637ee7caee2f
root <root@couch1.example.jp>
2016-03-31 23:36:57 +0900

    wip

---
+++ code/database/couchbase/users/clear.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from couchbase.bucket import *
+
+def _main():
+
+	b = Bucket('couchbase://127.0.0.1/USERS')
+
+	# truncate bucket
+	b.flush()
+
+_main()
+
