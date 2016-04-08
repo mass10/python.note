094da199a4624c077149e7342ad8974495f694c2
root <root@couch1.example.jp>
2016-04-08 19:51:02 +0900

    wip

--- code/database/couchbase/users/append-members.py
+++ code/database/couchbase/users/append-members.py
@@ -12,14 +12,14 @@ def _insert(bucket, user_name):
 
 def _main():
 
-	b = Bucket('couchbase://127.0.0.1/USERS')
-	insert = lambda user_name: _insert(b, user_name)
-	insert(u'岸部シロー')
-	insert(u'岸部一徳')
-	insert(u'沢田研二')
-	insert(u'加橋かつみ')
-	insert(u'森本太郎')
-	insert(u'瞳みのる')
+	users = Bucket('couchbase://127.0.0.1/USERS')
+
+	_insert(users, u'岸部シロー')
+	_insert(users, u'岸部一徳')
+	_insert(users, u'沢田研二')
+	_insert(users, u'加橋かつみ')
+	_insert(users, u'森本太郎')
+	_insert(users, u'瞳みのる')
 
 _main()
 
