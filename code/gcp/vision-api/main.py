e436b1f26b2acc8d08903a15843d85cb50926ef8
mass10 <irisawa.masaru@gmail.com>
2016-04-11 00:09:24 +0900

    wip

--- code/gcp/vision-api/main.py
+++ code/gcp/vision-api/main.py
@@ -13,7 +13,9 @@ from oauth2client.client import GoogleCredentials
 
 def main(photo_file):
 
-	API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
+	# =========================================================================
+	# 認証
+	# =========================================================================
 	http = httplib2.Http()
 	credentials = GoogleCredentials.get_application_default().create_scoped(
 		[
@@ -21,31 +23,39 @@ def main(photo_file):
 		]
 	)
 	credentials.authorize(http)
-	service = build(
-		'vision', 'v1', http, discoveryServiceUrl = API_DISCOVERY_FILE)
+
+	# =========================================================================
+	# リクエスト
+	# =========================================================================
+	API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
+	service = build('vision', 'v1', http, discoveryServiceUrl = API_DISCOVERY_FILE)
 	with open(photo_file, 'rb') as image:
 		image_content = base64.b64encode(image.read())
-		service_request = service.images().annotate(
-			body = {
-				'requests': [
-					{
-						'image': {
-							'content': image_content
-						},
-						'features': [
-							{
-								'type': 'LABEL_DETECTION',
-								'maxResults': 5,
-							}
-						]
-					}
-				]
-			}
-		)
+		body = {
+			'requests': [
+				{
+					'image': {
+						'content': image_content
+					},
+					'features': [
+						{
+							'type': 'LABEL_DETECTION',
+							'maxResults': 5,
+						}
+					]
+				}
+			]
+		}
+		service_request = service.images().annotate(body = body)
+
+	# =========================================================================
+	# 実行
+	# =========================================================================
 	response = service_request.execute()
-	print json.dumps(response, indent=4)
+	print json.dumps(response, indent = 4)
 
 if __name__ == '__main__':
+
 	parser = argparse.ArgumentParser()
 	parser.add_argument(
 		'--image-file', help = 'The image you\'d like to label.')
