70cd7543887e9be7d48852b40a1806b4598e305d
mass10 <irisawa.masaru@gmail.com>
2016-04-11 07:27:31 +0900

    wip

---
+++ code/gcp/vision-api/detect-text.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import argparse
+import base64
+import httplib2
+import json
+import pprint 
+# from apiclient.discovery import build
+from googleapiclient.discovery import build
+from oauth2client.client import GoogleCredentials
+
+def main(photo_file):
+
+	# =========================================================================
+	# 認証
+	# =========================================================================
+	http = httplib2.Http()
+	credentials = GoogleCredentials.get_application_default().create_scoped(
+		[
+			'https://www.googleapis.com/auth/cloud-platform',
+		]
+	)
+	credentials.authorize(http)
+
+	# =========================================================================
+	# リクエスト
+	# =========================================================================
+	API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
+	service = build('vision', 'v1', http, discoveryServiceUrl = API_DISCOVERY_FILE)
+	with open(photo_file, 'rb') as image:
+		image_content = base64.b64encode(image.read())
+		body = {
+			'requests': [
+				{
+					'image': {
+						'content': image_content
+					},
+					'features': [
+						{
+							'type': 'TEXT_DETECTION',
+							'maxResults': 5,
+						}
+					]
+				}
+			]
+		}
+		service_request = service.images().annotate(body = body)
+
+	# =========================================================================
+	# 実行
+	# =========================================================================
+	response = service_request.execute()
+	print json.dumps(response, indent = 4)
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'--image-file', help = 'The image you\'d like to label.')
+	args = parser.parse_args()
+	main(args.image_file)
