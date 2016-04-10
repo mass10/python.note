ec247943aa171a16371a460f624a26428b6d3a00
mass10 <irisawa.masaru@gmail.com>
2016-04-11 00:01:00 +0900

    wip

---
+++ code/gcp/vision-api/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import argparse
+import base64
+import httplib2
+import json
+import pprint 
+# from apiclient.discovery import build
+from googleapiclient.discovery import build
+from oauth2client.client import GoogleCredentials
+
+def main(photo_file):
+
+	API_DISCOVERY_FILE = 'https://vision.googleapis.com/$discovery/rest?version=v1'
+	http = httplib2.Http()
+	credentials = GoogleCredentials.get_application_default().create_scoped(
+		[
+			'https://www.googleapis.com/auth/cloud-platform',
+		]
+	)
+	credentials.authorize(http)
+	service = build(
+		'vision', 'v1', http, discoveryServiceUrl = API_DISCOVERY_FILE)
+	with open(photo_file, 'rb') as image:
+		image_content = base64.b64encode(image.read())
+		service_request = service.images().annotate(
+			body = {
+				'requests': [
+					{
+						'image': {
+							'content': image_content
+						},
+						'features': [
+							{
+								'type': 'LABEL_DETECTION',
+								'maxResults': 5,
+							}
+						]
+					}
+				]
+			}
+		)
+	response = service_request.execute()
+	print json.dumps(response, indent=4)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'--image-file', help = 'The image you\'d like to label.')
+	args = parser.parse_args()
+	main(args.image_file)
