679b5a7214e74e36fb0a29368de6518618a553b2
mass10 <irisawa.masaru@gmail.com>
2016-04-20 20:51:19 +0900

    wip

--- code/yield/main.py
+++ code/yield/main.py
@@ -3,12 +3,6 @@
 
 import sys
 
-def _println(*arguments):
-
-	for unknown in arguments:
-		sys.stdout.write((u'' + unknown).encode(u'utf-8'))
-	sys.stdout.write(u"\n")
-
 def _enumeration():
 
 	yield(u'1')
@@ -18,11 +12,7 @@ def _enumeration():
 
 def Main():
 
-	_println(u'### start ###')
-
-	for unknown in _enumeration():
-		_println(u'[', unknown, u']')
-
-	_println(u'--- end ---')
+	for e in _enumeration():
+		print e
 
 Main()
