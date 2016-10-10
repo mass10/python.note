a2b1ba13f8fbf0ed54724ba6c1f6012df5e1f2d5
mass10 <mass10.github@gmail.com>
2016-10-10 12:43:18 +0900

    wip

---
+++ code/signal/main2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import signal
+import time
+
+class application:
+
+	def __init__(self):
+		self.__alive = True
+	def _alive(self):
+		return self.__alive
+	def run(self):
+		print '### start ###'
+		while self._alive():
+			print '(running...)'
+			time.sleep(1)
+		print '--- end ---'
+	def quit(self):
+		self.__alive = False
+
+app = application()
+
+class main:
+
+	@staticmethod
+	def _handler(signum, frame):
+		app.quit()
+		print '$$$ interrupt $$$'
+	@staticmethod
+	def main():
+		signal.signal(signal.SIGINT, main._handler)
+		app.run()
+
+main.main()
