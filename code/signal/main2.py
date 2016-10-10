c4eea161089bfe870576f5b1db7bac2ff8776395
mass10 <mass10.github@gmail.com>
2016-10-10 12:46:49 +0900

    wip

--- code/signal/main2.py
+++ code/signal/main2.py
@@ -8,14 +8,17 @@ class application:
 
 	def __init__(self):
 		self.__alive = True
+
 	def _alive(self):
 		return self.__alive
+
 	def run(self):
 		print '### start ###'
 		while self._alive():
 			print '(running...)'
 			time.sleep(1)
 		print '--- end ---'
+
 	def quit(self):
 		self.__alive = False
 
@@ -27,6 +30,7 @@ class main:
 	def _handler(signum, frame):
 		app.quit()
 		print '$$$ interrupt $$$'
+
 	@staticmethod
 	def main():
 		signal.signal(signal.SIGINT, main._handler)
