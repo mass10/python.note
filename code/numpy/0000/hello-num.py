88a8a5b9d42ee4ff27b3d231655bcdec0d356da6
mass10 <mass10.github@gmail.com>
2017-07-06 14:03:05 +0000

    wip

--- code/numpy/0000/hello-num.py
+++ code/numpy/0000/hello-num.py
@@ -10,13 +10,13 @@ class Main:
 
 	def main(self):
 
-		print numpy.version.full_version
+		print(numpy.version.full_version)
 
 		xar = numpy.array([1, 2, 3, 4, 5])
-		print xar
-		print xar.ndim
-		print xar.shape
+		print(xar)
+		print(xar.ndim)
+		print(xar.shape)
 
-		print 'Ok.'
+		print('Ok.')
 
 Main().main()
