56a75e734b83816587cdf606738ebabcacba7407
mass10 <mass10.github@gmail.com>
2017-07-06 13:21:53 +0000

    wip

--- code/map/main.py
+++ code/map/main.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+def _bi(x):
+
+	return x * x
+
 def _main():
 
+	#
+	# ラムダ式
+	#
+
 	list = map(lambda i: i * i, [1, 2, 3, 4, 5])
-	print list
+
+	print(list)
+
+	#
+	# 普通の関数を呼び出す場合
+	#
+
+	list = map(_bi, [1, 2, 3, 4, 5])
+
+	print(list)
 
 _main()
 
