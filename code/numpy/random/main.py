d40c34dc90ae9f70f6e6952846fc6bf2c5070490
mass10 <mass10.github@gmail.com>
2017-11-03 08:07:46 +0000

    wip

--- code/numpy/random/main.py
+++ code/numpy/random/main.py
@@ -16,4 +16,16 @@ def main():
 	print('--- 0～1までの乱数を2列x10行生成 ---')
 	print(numpy.random.rand(10, 2))
 
+	print('')
+	print('--- 0～99までの整数を1つ生成 ---')
+	print(numpy.random.randint(0, 99))
+
+	print('')
+	print('--- 0～99までの整数を100個生成 ---')
+	print(numpy.random.randint(0, 99, 100))
+
+	print('')
+	print('--- 0～99までの整数を8列x2行生成 ---')
+	print(numpy.random.randint(0, 99, (2, 8)))
+
 main()
