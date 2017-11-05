5fb0f06d83b2f39c202cae052d947595870dcb43
mass10 <mass10.github@gmail.com>
2017-11-05 12:04:30 +0900

    wip

---
+++ code/sklearn/iris/show-iris-table.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding: utf-8
+import sklearn.datasets
+import pandas
+
+def main():
+
+	# アヤメのサンプルデータをテーブル状に表示する
+	data = sklearn.datasets.load_iris()
+	table = pandas.DataFrame(data.data, columns=data.feature_names)
+	print(table)
+
+main()
