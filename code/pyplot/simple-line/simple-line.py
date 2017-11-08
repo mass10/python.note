258323346a8d834ac40e67d85e7a06d4148c25b0
mass10 <mass10.github@gmail.com>
2017-11-08 21:24:51 +0900

    wip

--- code/pyplot/simple-line/simple-line.py
+++ code/pyplot/simple-line/simple-line.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot
 def main():
 
 	matplotlib.pyplot.rcParams['font.family'] = 'IPAPGothic'
-
-	matplotlib.pyplot.plot([1,2,3,4])
 	matplotlib.pyplot.ylabel('たてじく')
+	matplotlib.pyplot.xlabel('よこじく')
+	matplotlib.pyplot.plot([1,2,3,4])
 	# matplotlib.pyplot.show()
 	matplotlib.pyplot.savefig("simple.png")
 
