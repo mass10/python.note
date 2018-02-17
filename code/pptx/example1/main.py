5d42ae74494cebee721e7b086af93ef87f30f711
mass10 <mass10.github@gmail.com>
2018-02-17 12:02:39 +0900

    wip

--- code/pptx/example1/main.py
+++ code/pptx/example1/main.py
@@ -5,16 +5,50 @@ import pptx
 
 def main():
 
-	p = pptx.Presentation()
+	### Presentation ###
+	presentation = pptx.Presentation()
 
-	title_slide_layout = p.slide_layouts[0]
-	slide = p.slides.add_slide(title_slide_layout)
-	title = slide.shapes.title
-	subtitle = slide.placeholders[1]
+	### Title Slide ###
+	if True:
+		new_slide = presentation.slide_layouts[0]
+		slide = presentation.slides.add_slide(new_slide)
+		title = slide.shapes.title
+		title.text = "〇△□ 月次報告"
+		subtitle = slide.placeholders[1]
+		subtitle.text = "2018年 2月 14日\npowered by python-pptx"
 
-	title.text = "Hello, World!"
-	subtitle.text = "python-pptx was here!"
+	### Bullet Slide ###
+	if True:
+		new_slide = presentation.slide_layouts[1]
+		slide = presentation.slides.add_slide(new_slide)
+		shapes = slide.shapes
+		# タイトル部分
+		shapes.title.text = "Bullet Slide です"
+		# 下の部分
+		body_shape = shapes.placeholders[1]
+		pa = body_shape.text_frame.add_paragraph()
+		pa.text = "あいうえお"
+		pa = body_shape.text_frame.add_paragraph()
+		pa.text = "かきくけこ"
 
-	p.save('hello.pptx')
+	### Blank Slide
+	if True:
+		new_slide = presentation.slide_layouts[6]
+		slide = presentation.slides.add_slide(new_slide)
+		left = top = width = height = pptx.util.Inches(1)
+		textbox = slide.shapes.add_textbox(
+			pptx.util.Inches(0.8),
+			pptx.util.Inches(0.8),
+			pptx.util.Inches(3),
+			pptx.util.Inches(1))
+		text_frame = textbox.text_frame
+		text_frame.word_wrap = True
+		# 段落を追加
+		p = text_frame.add_paragraph()
+		run = p.add_run()
+		run.text = "Ansh ash a skjwidhnqm xnajhwslzm widh aslk al Najhsbleei. Le a Kan Najweee ka. Has ashqie nclk aiqw djfseok naxbad jqhdu asfifjnzxs kasjles jas wee le ag das."
+		run.font.color.rgb = pptx.dml.color.RGBColor(0x50, 0x80, 0x90)
+
+	presentation.save('hello.pptx')
 
 main()
