7baa50df804a50508284893e2d8eac1d966835b9
mass10 <mass10.github@gmail.com>
2018-02-17 16:33:44 +0900

    wip

--- code/pptx/example1/main.py
+++ code/pptx/example1/main.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import os
+import json
 import pptx
+import pptx.chart.data
+
+def load_json_as_python_object(path):
+	
+	with open(path) as file:
+		return json.load(file)
+
+def load_chart_data():
+	
+	unknown = load_json_as_python_object("data/data.json")
+
+	labels = []
+	series_01 = []
+	series_02 = []
+	for e in unknown:
+		labels.append(e["label"])
+		series_01.append(e["2016"])
+		series_02.append(e["2017"])
+	return labels, series_01, series_02
 
 def main():
+
+	### loading chart data ###
+	series_labels_01, series_01, series_02 = load_chart_data()
 
 	### Presentation ###
 	presentation = pptx.Presentation()
@@ -47,6 +71,27 @@ def main():
 		pa.text = "上司がパワポが好き"
 		pa = body_shape.text_frame.add_paragraph()
 		pa.text = "毎月パワポで報告作れ"
+
+	### Bullet Slide ###
+	if True:
+
+		new_slide = presentation.slide_layouts[1]
+
+		slide = presentation.slides.add_slide(new_slide)
+		shapes = slide.shapes
+		# タイトル部分
+		shapes.title.text = "年間平均気温(2017年)"
+		# チャート
+		chart_data = pptx.chart.data.ChartData()
+		chart_data.categories = series_labels_01
+		chart_data.add_series('2016年', series_01)
+		chart_data.add_series('2017年', series_02)
+		x, y = pptx.util.Inches(1.0), pptx.util.Inches(2)
+		cx, cy = pptx.util.Inches(8), pptx.util.Inches(4.5)
+		chart = slide.shapes.add_chart(
+			pptx.enum.chart.XL_CHART_TYPE.COLUMN_CLUSTERED, x, y, cx, cy, chart_data
+		).chart
+		chart.has_legend = True
 
 	### Bullet Slide ###
 	if True:
