536e91207170360f79d89da0442cb642b4dd65c0
mass10 <mass10.github@gmail.com>
2018-03-07 22:31:03 +0900

    wip

--- code/database/elasticsearch/examples/hello.py
+++ code/database/elasticsearch/examples/hello.py
@@ -4,7 +4,7 @@
 # elasticsearch はじめの一歩 with Python
 #
 #
-# pip install elasticsearch
+# pip3 install elasticsearch
 #
 
 import sys
@@ -14,21 +14,32 @@ from datetime import datetime
 from elasticsearch import Elasticsearch
 import json
 
+
+def _get_insert(connection, index_name, doc_type):
+	def _operation(name, maker):
+		connection.index(index = index_name, doc_type = doc_type, id = uuid.uuid4(),
+			body = {"name": name, "maker": maker})
+	return _operation
+
 def _main(argv):
 
-	INDEX_NAME = 'sample_database_20151123'
-	DOC_TYPE_NAME = 'sample_entry_20151123',
-
-
+	INDEX_NAME = 'ゲームインデックス'
+	DOC_TYPE_NAME = 'ゲーム',
 
 	# 接続設定
 	# es = Elasticsearch()
 	es = Elasticsearch(host = '127.0.0.1')
 
+	insert = _get_insert(es, INDEX_NAME, DOC_TYPE_NAME)
+
 	# index に document(?) を追加
-	for e in range(100):
-		es.index(index = INDEX_NAME, doc_type = DOC_TYPE_NAME, id = str(uuid.uuid1()), body = {})
-		print('.')
+	insert("いっき", "サンソフト")
+	insert("ツインビー", "コナミ")
+	insert("スペランカー", "アイレム")
+	insert("ゼビウス", "ナムコ")
+	insert("ゼビウス", "ナムコ")
+	insert("謎の村雨城", "コナミ")
+	insert("ゼルダの伝説", "任天堂")
 
 	# 強制コミット
 	es.indices.refresh(index = INDEX_NAME)
@@ -37,16 +48,19 @@ def _main(argv):
 
 	# 全件検索
 	# response = es.search(index = INDEX_NAME, body = {})
-	response = es.search(index = INDEX_NAME, body={"query": {"match_all": {}}, "size": 9999})
+	response = es.search(
+		index = INDEX_NAME,
+		body={"query": {"match_all": {}}, "size": 9999})
 
 	# 結果を表示
 	for e in response['hits']['hits']:
-		print(json.dumps(e))
+		print(json.dumps(e, ensure_ascii = False))
 
 	# ヒット数
 	print('hits: {0}'.format(response['hits']['total']))
 
 	# index(=データベース？？)を破棄
-	es.indices.delete(index = INDEX_NAME)
+	if es.indices.exists(index = "人名インデックス"):
+		es.indices.delete(index = INDEX_NAME)
 
 _main(sys.argv)
