285ae3642b50e276be18d2b864440d4200faf4cd
mass10 <mass10.github@gmail.com>
2018-06-28 19:27:24 +0900

    wip

--- code/networking/smtp/main.py
+++ code/networking/smtp/main.py
@@ -8,16 +8,23 @@ from email.Utils import formatdate
 
 def _main():
 
-	subject = 'this is subjet...'
-	mail_from = 'aaa@example.jp'
-	rcpt_to = 'bbb@example.jp'
-	body = 'hello-------'
+	subject = "テストです(^。^)y-.。o○"
+	mail_from = "ec2-user"
+	rcpt_to = "ec2-user"
+	body = """hello-------
+o
+o
+o
+o
+...........
+"""
 
 	message = MIMEText(body)
-	message['Subject'] = subject
+	message['Subject'] = smtplib.email.Header.Header(subject, "utf-8")
 	message['From'] = mail_from
 	message['To'] = rcpt_to
 	message['Date'] = formatdate()
+	message.add_header('Content-Type', 'text/plain; charset=UTF-8')
 
 	s = smtplib.SMTP('127.0.0.1:25')
 	s.sendmail(mail_from, [rcpt_to], message.as_string())
