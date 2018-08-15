ec8fb1195a6e7a4a19c2b2c36ef2d3c14ee8bacc
mass10 <mass10.github@gmail.com>
2018-08-15 14:32:49 +0900

    wip

---
+++ code/stdin/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import os
+import io
+
+
+def _main():
+
+    file = sys.stdin
+    while True:
+        line = file.readline()
+        if line == '':
+            break
+        line = line.rstrip("\r\n")
+        print(u'[{0}]'.format(line))
+    file.close()
+
+_main()
+
