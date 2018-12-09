c942fb87e9dc63e41b65d8ad565b6401df5645a8
mass10 <mass10.github@gmail.com>
2018-12-09 13:09:18 +0900

    wip

--- code/yield/main.py
+++ code/yield/main.py
@@ -1,18 +1,37 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 import sys
+import inspect
+
+def _log(*args):
+
+	curframe = inspect.currentframe()
+	calframe = inspect.getouterframes(curframe, 2)
+	caller = calframe[1][3]
+	print("[TRACE] <", caller, "> ", *args, sep="")
 
 def _enumeration():
 
+	_log("いまから \"1\" を返します。")
 	yield(u'1')
+
+	_log("いまから \"2\" を返します。")
 	yield(u'2')
+
+	_log("いまから \"3\" を返します。")
 	yield(u'3')
+
+	_log("いまから \"あいうえお\" を返します。")
 	yield(u'あいうえお')
 
-def Main():
+	_log("返すものはありません。")
 
+def _main():
+
+	_log("### start ###")
 	for e in _enumeration():
-		print e
+		_log("CURRENT: [", e, "]")
+	_log("--- end ---")
 
-Main()
+_main()
