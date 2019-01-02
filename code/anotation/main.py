0e91efa7f1777dfa128a38222526ef6bc5c53410
mass10 <mass10.github@gmail.com>
2019-01-02 12:08:24 +0900

    wip

--- code/anotation/main.py
+++ code/anotation/main.py
@@ -4,8 +4,13 @@
 import mylib.decorator
 
 @mylib.decorator.mydecorator
+def _test(left, right):
+	pass
+
+@mylib.decorator.mydecorator
 def _main():
 	print("### start ###")
+	_test("aaa", "bbb")
 	print("--- end ---")
 
 _main()
--- code/anotation/mylib/decorator.py
+++ code/anotation/mylib/decorator.py
@@ -1,4 +1,5 @@
 def mydecorator(func):
 	def inner_func(*args):
+		print("[TRACE] <annotation> call: ", func, ", arguments: ", args, "", sep="")
 		func(*args)
 	return inner_func
